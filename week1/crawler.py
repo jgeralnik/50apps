a6e6125346b981d4009075593dcc48bf27c874fb
Joey Geralnik <jgeralnik@gmail.com>
2012-02-01 15:16:02 +0200

    Using argparse to parse command line args

--- week1/crawler.py
+++ week1/crawler.py
@@ -3,6 +3,8 @@ from lxml.html.clean import clean_html
 from lxml.html.soupparser import fromstring
 import requests
 from Queue import Queue
+from termcolor import colored
+import argparse
 
 def crawl(url, depth, search):
   visited = set()
@@ -16,18 +18,28 @@ def crawl(url, depth, search):
     try:
       r = requests.get(url)
     except ValueError:
+      print "Url is invalid:", url 
       return #url is not valid
+    except requests.exceptions.ConnectionError:
+      print "Error connecting to", url
+      return
 
     if r.status_code != 200:
+      print "Status code is", r.status_code, "on", url
       return
 
     try:
       html = fromstring(r.content)
     except ValueError:
+      print "ValueError on", url
+      return    #Content is not html
+      
+    except TypeError:
+      print "TypeError on", url 
       return    #Content is not html
 
     html = clean_html(html)
-    if search in html.text_content():
+    if search.lower() in html.text_content().lower():
       result.add(url)
 
     if depth==0:
@@ -35,7 +47,8 @@ def crawl(url, depth, search):
 
     html.make_links_absolute(url)
     for element, attr, link, pos in html.iterlinks():
-      to_visit.put((link, depth-1))
+      if attr == "href":
+        to_visit.put((link, depth-1))
 
 
   to_visit.put((url, depth))
@@ -46,5 +59,10 @@ def crawl(url, depth, search):
   return result
 
 if __name__ == "__main__":
-  for url in crawl(sys.argv[1],int(sys.argv[2]), sys.argv[3]):
-    print url
+  parser = argparse.ArgumentParser(description="Crawl the web for search query")
+  parser.add_argument('url', help='The url to start the search from')
+  parser.add_argument('query', help='The string to search for')
+  parser.add_argument('-d', '--depth', type=int, help='The depth to search', default=1)
+  args = parser.parse_args()
+  for url in crawl(args.url,args.depth,args.query):
+    print colored(url, 'red')
